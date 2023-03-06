746c8108cca7dd393c1c7d9aeb68222b299f53bb
Leon Martin <lema@work>
2023-03-06 19:53:42 +0100

    fixed bug in benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -25,9 +25,9 @@ def runtime(runs=100):
     """
 
     configs = {
-        "$R_{i,e} + L$": "python3 ./preprocessor.py run True True",
-        "$R_{i} + L$": "python3 ./preprocessor.py run True False",
-        "$R_{e} + L$": "python3 ./preprocessor.py run False True",
+        "$R_{i,e} + L$": "python3 ./preprocessor.py True True",
+        "$R_{i} + L$": "python3 ./preprocessor.py True False",
+        "$R_{e} + L$": "python3 ./preprocessor.py False True",
         "$L$": "",
     }
 
@@ -52,7 +52,7 @@ def runtime(runs=100):
                 rdftex_duration = t1_stop - t1_start
             else:
                 # this has to be run to make tex compilable
-                subprocess.run(["python3 ./preprocessor.py run False False"], capture_output=True, shell=True)
+                subprocess.run(["python3 ./preprocessor.py False False"], capture_output=True, shell=True)
                 rdftex_duration = 0
 
             # benchmark latexmk
@@ -65,27 +65,27 @@ def runtime(runs=100):
             logging.info(f"{config} - Run {ctr + 1} completed. RDFtex/Latexmk took {rdftex_duration}/{latexmk_duration} seconds.")
 
     # stacked bar chart of average runtimes
-    average_latexmk = [statistics.mean([latexmk_time for _, latexmk_time in times]) for times in results.values()]
-    average_rdftex = [statistics.mean([rdftex_time for rdftex_time, _ in times]) for times in results.values()]
+    median_latexmk = [statistics.median([latexmk_time for _, latexmk_time in times]) for times in results.values()]
+    median_rdftex = [statistics.median([rdftex_time for rdftex_time, _ in times]) for times in results.values()]
 
-    average_results = {
-        "Latexmk": average_latexmk,
-        "RDFtex": average_rdftex,
+    median_results = {
+        "Latexmk": median_latexmk,
+        "RDFtex": median_rdftex,
     }
 
     fig, ax = plt.subplots()
     bottom = [0 for _ in range(len(configs))]
 
-    for which_average, average_result in average_results.items():
+    for which_average, median_result in median_results.items():
         color = "white" if which_average == "Latexmk" else "black"
-        p = ax.bar(configs.keys(), average_result, label=which_average, color=color, edgecolor="black", bottom=bottom)
-        bottom = [b + w for b, w in zip(bottom, average_result)]
+        p = ax.bar(configs.keys(), median_result, label=which_average, color=color, edgecolor="black", bottom=bottom)
+        bottom = [b + w for b, w in zip(bottom, median_result)]
 
     ax.set_ylabel("Seconds")
     ax.legend(loc="lower left")
 
-    fig.savefig(f"benchmark-runtime-bar-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
-    fig.savefig(f"benchmark-runtime-bar-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
+    fig.savefig(f"./benchmark-results/benchmark-runtime-bar-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
+    fig.savefig(f"./benchmark-results/benchmark-runtime-bar-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
 
     # box plot of runtimes
     aggregated_results = {scenario: [r + l for r, l in times] for scenario, times in results.items()}
@@ -98,8 +98,8 @@ def runtime(runs=100):
     for patch in boxplot["boxes"]:
         patch.set_facecolor("black")
 
-    fig.savefig(f"benchmark-runtime-box-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
-    fig.savefig(f"benchmark-runtime-box-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
+    fig.savefig(f"./benchmark-results/benchmark-runtime-box-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
+    fig.savefig(f"./benchmark-results/benchmark-runtime-box-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
 
 
 def response_times(runs=100):
@@ -164,8 +164,8 @@ def response_times(runs=100):
     for patch in boxplot["boxes"]:
         patch.set_facecolor("black")
 
-    fig.savefig(f"benchmark-response-box-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
-    fig.savefig(f"benchmark-response-box-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
+    fig.savefig(f"./benchmark-results/benchmark-response-box-{PROJECT_DIR.replace('/', '')}-{runs}.eps", bbox_inches="tight", format="eps")
+    fig.savefig(f"./benchmark-results/benchmark-response-box-{PROJECT_DIR.replace('/', '')}-{runs}.pdf", bbox_inches="tight")
 
 
 if __name__ == "__main__":
